2359e46e0b6e96663b534074c2804c5b1a5df630
TheMiner_02 <theminer02mail@gmail.com>
2021-12-19 19:41:27 +0100

    testbot real ping

--- commands/testbot.js
+++ commands/testbot.js
@@ -1,18 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-function getPing() {
-  // its not real o.O :shhh:
-  generatedPing = Math.floor(Math.random() * 44 + 1);
-  ping = generatedPing + "ms";
-  return ping;
-}
-
 module.exports = {
 	data: new SlashCommandBuilder()
 	.setName('testbot')
 	.setDescription('Checks if the bot is online'),
-	async execute(interaction) {
-		await interaction.reply("Bot is online. **" + getPing() + "**");
+	execute: async (interaction, client) => {
+    const msg = await interaction.reply({ content: "Bot is **online** ✅", fetchReply: true });
+
+    await interaction.editReply({ content: `Bot is **online** ✅\n__⏳ Latency__\n- Bot Latency: \`${msg.createdTimestamp - interaction.createdTimestamp}ms\`\n- WebSocket Latency: \`${Math.round(client.ws.ping)}ms\`` });
     console.log("testbot - Answer sent");
 	},
 };
