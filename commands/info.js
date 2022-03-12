16105ed5761af23c5b091ae6fa3e53be345ee570
TheMiner_02 <dev@theminer02.com>
2022-03-12 20:21:57 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -33,7 +33,7 @@ module.exports = {
     text_links =
       "**__My social media__**\n**YouTube:** <https://youtube.com/theminer02>\n**Instagram:** <https://instagram.com/theminer_02>\n**Twitter:** <https://twitter.com/theminer_02>\n**PlanetMinecraft:** <https://planetminecraft.com/member/theminer02>\n**Website:** <https://theminer02.com/>\n**Twitch *(german)*:** <https://www.twitch.tv/theminer_02>";
     text_bot =
-      "**__Bot__**\n**Commands:** Use `/help`\n**General Info:** I made this bot on my own and its completely customized for me. You can't use it on your own server.\n**Version:** " + botVersion + "\n**Nerd stuff:** ~" + codeLines + " lines of code, ~" + workHours + " hours of work - I'm using `" + discordjs + "` & the bot is currently hosted on Replit\n**Uptime:** <https://stats.uptimerobot.com/rVzlqsrnNL/787785032>";
+      "**__Bot__**\n**Commands:** Use `/help`\n**General Info:** I made this bot on my own and its completely customized for me. You can't use it on your own server.\n**Version:** " + botVersion + "\n**Nerd stuff:** ~" + codeLines + " lines of code, ~" + workHours + " hours of work - I'm using `" + discordjs + "`\n**Uptime:** <https://stats.uptimerobot.com/rVzlqsrnNL/787785032>\n**GitHub-Repo:** <https://github.com/theminer02/tmbot-js>";
     text_donate =
       "**__Donate__**\nI don't know why you would want to donate something, but if you do, here you go:\n<https://www.tipeeestream.com/theminer02/donation>";
     text_builder =
